1b05d967fabea03ce63f2deab2a8fcf80322ec4d
Caio Sousa <cmcaiomello@gmail.com>
2025-07-27 21:24:07 -0300

    troca

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-console.log("oi em breve serei uma api")
+import express from 'express';
+const servidor = express();
 
+
+servidor.get('/helloworld', (res, resp ) =>{
+
+   resp.send('helloworld!!! :)')
+
+})
+
+
+
+
+servidor.listen(5001, () => console.log('Api subida com sucesso'))
 
