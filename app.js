6116a0b9f396eb613099503665da3fd3b25f0ff0
Caio Sousa <cmcaiomello@gmail.com>
2025-07-28 16:45:45 -0300

    troca

--- app.js
+++ app.js
@@ -9,13 +9,24 @@ servidor.get('/helloworld', (req, resp ) =>{
 
 })
 
-servidor.get('/mensagem/boasvindas', (re1, resp) =>{
+servidor.get('/mensagem/boasvindas', (req, resp) =>{
 
 
 resp.send('Olá, sejam bem-vindos e bem-vindas!')
 
 })
 
+servidor.get('/v2/mensagem/boasvindas', (req, resp) =>{
+
+   resp.send('Que bom que você está aqui! s2')
+})
+
+servidor.get('/mensagem/ocupado', (req, resp) =>{
+
+
+resp.send('Estou ocupado no momento!')
+
+})
 
 
 servidor.listen(5001, () => console.log('Api subida com sucesso'))
